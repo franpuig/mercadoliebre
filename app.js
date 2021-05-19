78e38e5d01fc19d4f2c44446339e2d9e215d9b40
franpuig <82483985+franpuig@users.noreply.github.com>
2021-05-18 23:06:33 -0300

    mercadoliebre

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require ("express");
+const app = express ();
+const path = require ("path");
+const publicPatch = path.resolve (__dirname,"./public")
+app.use( express.static(publicPatch));
+
+const port = process.env.PORT || 3030;
+
+app.listen (port, ()=>{
+console.log ("Mi servidor express esta funcionando en port 3030")
+});
+
+app.get('/', (req, res) => {
+    res.sendFile (path.resolve (__dirname, "./views/home.html"))
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile (path.resolve (__dirname, "./views/register.html"))
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile (path.resolve (__dirname, "./views/login.html"))
+});
+
+
